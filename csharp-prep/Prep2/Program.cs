43941dc7f1bd0f50029bc3758ee23c4407c5d0b2
Elijah Dodge <ej_6612@outlook.com>
2023-02-26 20:36:49 -0700

    Completed Prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello Prep2 World!"); 
+
+        Console.Write("What is your grade? ");
+        float grade = float.Parse(Console.ReadLine());
+
+        string letter;
+        string classStatus;
+
+        // A
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        // B
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        // C
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        // D
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        // F
+        else
+        {
+            letter = "F";
+        }
+
+        // PASS OR FAIL
+        if (grade >= 70)
+        {
+            classStatus = "PASS";
+        }
+
+        else 
+        {
+            classStatus = "FAIL";
+        }
+
+        Console.WriteLine($"Your grade is {letter}. STATUS: {classStatus}");
+
     }
 }
