181930134cfc66dad3180be9729729a4dc79b7c4
Elijah Dodge <ej_6612@outlook.com>
2023-03-13 14:30:43 -0600

    Learning 03 Completed.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Fraction
+{
+    private int _top, _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        double result = (double)_top / (double)_bottom;
+        return result;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(1, 3);
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
     }
+
+
 }
