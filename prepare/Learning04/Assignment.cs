e52834b25462c579e8ad3093596882145aeab162
Elijah Dodge <ej_6612@outlook.com>
2023-03-14 21:54:18 -0600

    Completed W04 Prepare

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} — {_topic}";
+    }
+
+}
+
+
+class MathAssignment : Assignment
+{
+    private string _textbookSection, _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+
+}
+
+
+class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+
+        Assignment basics1 = new Assignment("Elijah Dodge", "Multiplication");
+        MathAssignment maths1 = new MathAssignment("Elijah Dodge", "Fractions", "Section 7.3", "Problems 8-19");
+        WritingAssignment writings1 = new WritingAssignment("Elijah Dodge", "History of Ducks", "History of Ducks VOL XXIII: The Cold War");
+
+        Console.WriteLine(basics1.GetSummary());
+        Console.WriteLine();
+        Console.WriteLine(maths1.GetSummary());
+        Console.WriteLine(maths1.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(writings1.GetSummary());
+        Console.WriteLine(writings1.GetWritingInformation());
+        
     }
 }
