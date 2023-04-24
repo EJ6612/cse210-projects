a768f4fd30259509993a5680b8a382953c9c33ca
Elijah Dodge <ej_6612@outlook.com>
2023-04-24 14:47:33 -0600

    Placed Learning03 files into own namespace Disabled ImplicitUsings

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,38 +1,41 @@
 using System;
 
-class Fraction
+namespace Learning03
 {
-    private int _top, _bottom;
-
-    public Fraction()
+    class Fraction
     {
-        _top = 1;
-        _bottom = 1;
+        private int _top, _bottom;
 
-    }
+        public Fraction()
+        {
+            _top = 1;
+            _bottom = 1;
 
-    public Fraction(int wholeNumber)
-    {
-        _top = wholeNumber;
-        _bottom = 1;
+        }
 
-    }
+        public Fraction(int wholeNumber)
+        {
+            _top = wholeNumber;
+            _bottom = 1;
 
-    public Fraction(int top, int bottom)
-    {
-        _top = top;
-        _bottom = bottom;
+        }
 
-    }
+        public Fraction(int top, int bottom)
+        {
+            _top = top;
+            _bottom = bottom;
 
-    public string GetFractionString()
-    {
-        return $"{_top}/{_bottom}";
-    }
+        }
 
-    public double GetDecimalValue()
-    {
-        double result = (double)_top / (double)_bottom;
-        return result;
+        public string GetFractionString()
+        {
+            return $"{_top}/{_bottom}";
+        }
+
+        public double GetDecimalValue()
+        {
+            double result = (double)_top / (double)_bottom;
+            return result;
+        }
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,23 +1,26 @@
 using System;
 
-class Program
+namespace Learning03
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Learning03 World!");
 
-        Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(5);
-        Fraction fraction3 = new Fraction(1, 3);
+            Fraction fraction1 = new Fraction();
+            Fraction fraction2 = new Fraction(5);
+            Fraction fraction3 = new Fraction(1, 3);
 
-        Console.WriteLine(fraction1.GetFractionString());
-        Console.WriteLine(fraction1.GetDecimalValue());
-        Console.WriteLine(fraction2.GetFractionString());
-        Console.WriteLine(fraction2.GetDecimalValue());
-        Console.WriteLine(fraction3.GetFractionString());
-        Console.WriteLine(fraction3.GetDecimalValue());
+            Console.WriteLine(fraction1.GetFractionString());
+            Console.WriteLine(fraction1.GetDecimalValue());
+            Console.WriteLine(fraction2.GetFractionString());
+            Console.WriteLine(fraction2.GetDecimalValue());
+            Console.WriteLine(fraction3.GetFractionString());
+            Console.WriteLine(fraction3.GetDecimalValue());
+
+        }
+
 
     }
-
-
 }
