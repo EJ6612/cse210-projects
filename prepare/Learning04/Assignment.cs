eb469c24c33d9508357505a0c9325233796b13e3
Elijah Dodge <ej_6612@outlook.com>
2023-04-24 14:54:55 -0600

    Disabled ImplicitUsings Placed Learning04 into it's own namespace

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,54 +1,57 @@
 using System;
 
-class Assignment
-{
-    protected string _studentName;
-    private string _topic;
-
-    public Assignment(string studentName, string topic)
+namespace Learning04 
+{    
+    class Assignment
     {
-        _studentName = studentName;
-        _topic = topic;
+        protected string _studentName;
+        private string _topic;
+
+        public Assignment(string studentName, string topic)
+        {
+            _studentName = studentName;
+            _topic = topic;
+        }
+
+        public string GetSummary()
+        {
+            return $"{_studentName} — {_topic}";
+        }
+
     }
 
-    public string GetSummary()
+
+    class MathAssignment : Assignment
     {
-        return $"{_studentName} — {_topic}";
+        private string _textbookSection, _problems;
+
+        public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+        {
+            _textbookSection = textbookSection;
+            _problems = problems;
+        }
+
+        public string GetHomeworkList()
+        {
+            return $"{_textbookSection} {_problems}";
+        }
+
     }
 
-}
 
-
-class MathAssignment : Assignment
-{
-    private string _textbookSection, _problems;
-
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    class WritingAssignment : Assignment
     {
-        _textbookSection = textbookSection;
-        _problems = problems;
+        private string _title;
+
+        public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+        {
+            _title = title;
+        }
+
+        public string GetWritingInformation()
+        {
+            return $"{_title} by {_studentName}";
+        }
+
     }
-
-    public string GetHomeworkList()
-    {
-        return $"{_textbookSection} {_problems}";
-    }
-
-}
-
-
-class WritingAssignment : Assignment
-{
-    private string _title;
-
-    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
-    {
-        _title = title;
-    }
-
-    public string GetWritingInformation()
-    {
-        return $"{_title} by {_studentName}";
-    }
-
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,23 +1,26 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Learning04 
+{    
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
-        Console.Clear();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Learning04 World!");
+            Console.Clear();
 
-        Assignment basics1 = new Assignment("Elijah Dodge", "Multiplication");
-        MathAssignment maths1 = new MathAssignment("Elijah Dodge", "Fractions", "Section 7.3", "Problems 8-19");
-        WritingAssignment writings1 = new WritingAssignment("Elijah Dodge", "History of Ducks", "History of Ducks VOL XXIII: The Cold War");
+            Assignment basics1 = new Assignment("Elijah Dodge", "Multiplication");
+            MathAssignment maths1 = new MathAssignment("Elijah Dodge", "Fractions", "Section 7.3", "Problems 8-19");
+            WritingAssignment writings1 = new WritingAssignment("Elijah Dodge", "History of Ducks", "History of Ducks VOL XXIII: The Cold War");
 
-        Console.WriteLine(basics1.GetSummary());
-        Console.WriteLine();
-        Console.WriteLine(maths1.GetSummary());
-        Console.WriteLine(maths1.GetHomeworkList());
-        Console.WriteLine();
-        Console.WriteLine(writings1.GetSummary());
-        Console.WriteLine(writings1.GetWritingInformation());
-        
+            Console.WriteLine(basics1.GetSummary());
+            Console.WriteLine();
+            Console.WriteLine(maths1.GetSummary());
+            Console.WriteLine(maths1.GetHomeworkList());
+            Console.WriteLine();
+            Console.WriteLine(writings1.GetSummary());
+            Console.WriteLine(writings1.GetWritingInformation());
+            
+        }
     }
 }
