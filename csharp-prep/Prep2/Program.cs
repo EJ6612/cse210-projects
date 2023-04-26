d837eb6717cd051fa531a16305d2d7e0fc350163
Elijah Dodge <ej_6612@outlook.com>
2023-04-26 14:17:04 -0600

    namespaced; disabled implicit usings

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,59 +1,62 @@
 using System;
 
-class Program
+namespace Prep2 
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!"); 
-
-        Console.Write("What is your grade? ");
-        float grade = float.Parse(Console.ReadLine());
-
-        string letter;
-        string classStatus;
-
-        // A
-        if (grade >= 90)
+        static void Main(string[] args)
         {
-            letter = "A";
+            Console.WriteLine("Hello Prep2 World!"); 
+
+            Console.Write("What is your grade? ");
+            float grade = float.Parse(Console.ReadLine());
+
+            string letter;
+            string classStatus;
+
+            // A
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+
+            // B
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+
+            // C
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+
+            // D
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+
+            // F
+            else
+            {
+                letter = "F";
+            }
+
+            // PASS OR FAIL
+            if (grade >= 70)
+            {
+                classStatus = "PASS";
+            }
+
+            else 
+            {
+                classStatus = "FAIL";
+            }
+
+            Console.WriteLine($"Your grade is {letter}. STATUS: {classStatus}");
+
         }
-
-        // B
-        else if (grade >= 80)
-        {
-            letter = "B";
-        }
-
-        // C
-        else if (grade >= 70)
-        {
-            letter = "C";
-        }
-
-        // D
-        else if (grade >= 60)
-        {
-            letter = "D";
-        }
-
-        // F
-        else
-        {
-            letter = "F";
-        }
-
-        // PASS OR FAIL
-        if (grade >= 70)
-        {
-            classStatus = "PASS";
-        }
-
-        else 
-        {
-            classStatus = "FAIL";
-        }
-
-        Console.WriteLine($"Your grade is {letter}. STATUS: {classStatus}");
-
     }
 }
