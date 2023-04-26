b9ae90c048c865785f2765f791d48956b35ec4ea
Elijah Dodge <ej_6612@outlook.com>
2023-04-26 14:20:17 -0600

    namespaced; disabled implicit usings

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,48 +1,51 @@
 using System;
 
-class Program
+namespace Prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Prep5 World!");
 
-        DisplayWelcome();
-        string name = PromptUserName();
-        int number = PromptUserNumber();
-        DisplayResult(name, number);
+            DisplayWelcome();
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            DisplayResult(name, number);
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");        
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squaredNumber = number * number;
+            return squaredNumber;
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            int sqNum = SquareNumber(number);
+            Console.WriteLine($"{name}, the square of your number is {sqNum}.");
+        }
+
     }
-
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the program!");        
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
-
-        return name;
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-
-        return number;
-    }
-
-    static int SquareNumber(int number)
-    {
-        int squaredNumber = number * number;
-        return squaredNumber;
-    }
-
-    static void DisplayResult(string name, int number)
-    {
-        int sqNum = SquareNumber(number);
-        Console.WriteLine($"{name}, the square of your number is {sqNum}.");
-    }
-
 }
