03c3603e34207717fcb2b4a4bb5ec057c20531a9
Elijah Dodge <ej_6612@outlook.com>
2023-07-10 15:11:35 -0600

    Random stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        int groupSize = 4;
+        int totalGroups = 3;
+        int currentNumber = 1;
+
+        for (int i = 0; i < totalGroups; i++)
+        {
+            for (int j = 0; j < groupSize; j++)
+            {
+                Console.Write(currentNumber + " ");
+                currentNumber += 2;
+            }
+            Console.Write(" ");
+
+            for (int j = 0; j < groupSize; j++)
+            {
+                Console.Write(currentNumber + " ");
+                currentNumber += 2;
+            }
+            Console.WriteLine();
+        }
     }
+
+    
 }
